b1f0b3e71e19d47a7bf7c1025bd29f08643df5d6
Michal Sulkiewicz <michals@mac.com>
2021-03-17 10:38:21 +0100

    Initial Commit

---
+++ Books2ReadWDI21/Books2ReadWDI21App.swift
@@ -0,0 +1,17 @@
+//
+//  Books2ReadWDI21App.swift
+//  Books2ReadWDI21
+//
+//  Created by Michal Sulkiewicz on 17/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Books2ReadWDI21App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
